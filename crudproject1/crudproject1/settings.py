d18da46e05590226f910da0c8dd381095afdb7e9
chaoticCalm <vaishalisaini0203@gmail.com>
2023-03-14 18:10:01 +0530

    api

--- crudproject1/crudproject1/settings.py
+++ crudproject1/crudproject1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'enroll',
 ]
 
@@ -118,7 +119,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#REST_FRAMEWORK = {
+ #   'DEFAULT_RENDERER_CLASSES':('rest_framework.renderers.JSONRenderer',)
+#}
--- crudproject1/crudproject1/urls.py
+++ crudproject1/crudproject1/urls.py
@@ -1,6 +1,6 @@
  
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from enroll import views
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('',views.add_show,name='addandShow'),
     path('delete/<int:id>',views.delete_data,name="deleteData"),
     path('update/<int:id>',views.update_data,name="updateData"),
+    path('api/', include('enroll.api.urls')),
 
 ]
